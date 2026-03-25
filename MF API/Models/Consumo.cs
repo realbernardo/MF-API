6d9cca1f55674917b85d297406fec377dd4ea6ed
Bernardo <hoelzleace@gmail.com>
2026-03-24 21:40:34 -0300

    Adicionar arquivos de projeto.

---
+++ MF API/Models/Consumo.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MF_API.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+    }
+
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
