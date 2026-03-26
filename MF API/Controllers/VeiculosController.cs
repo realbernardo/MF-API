d121aff68b21d1637e9b60abde60903353a727a3
Bernardo <hoelzleace@gmail.com>
2026-03-26 18:20:02 -0300

    addusuario removeusuario relação veiculos

--- MF API/Controllers/VeiculosController.cs
+++ MF API/Controllers/VeiculosController.cs
@@ -49,7 +49,7 @@ namespace MF_API.Controllers
         public async Task<ActionResult> GetById(int id)
         {
 
-            var model = await _context.Veiculos.Include(t => t.Consumos).FirstOrDefaultAsync(c => c.Id == id);
+            var model = await _context.Veiculos.Include(t => t.Usuarios).ThenInclude(t => t.Usuario).Include(t => t.Consumos).FirstOrDefaultAsync(c => c.Id == id);
            
             if(model == null)
             {
@@ -108,5 +108,37 @@ namespace MF_API.Controllers
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "DELETE"));
 
         }
+
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int Id, VeiculoUsuarios model)
+        {
+            if (Id != model.VeiculoId)
+            {
+                return BadRequest(new { message = "Id do veículo não corresponde ao id da URL" });
+            }
+
+            _context.VeiculosUsuarios.Add(model);
+            await  _context.SaveChangesAsync();
+            return CreatedAtAction("GetById", new {id = model.VeiculoId}, model);
+
+        }
+
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int Id, int usuarioId)
+        {
+            
+            var model = await _context.VeiculosUsuarios.Where(c => c.VeiculoId == Id && c.UsuarioId == usuarioId).FirstOrDefaultAsync();
+
+            if (model == null)
+            {
+                return NotFound(new { message = "Relação veículo-usuário não encontrada" });
+            }
+
+            _context.VeiculosUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
     }
 }
