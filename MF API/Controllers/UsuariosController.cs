ecb64b429f2fd9957c66d0c4e9cfab36b276b8a0
Bernardo <hoelzleace@gmail.com>
2026-03-27 11:54:44 -0300

    configurando acesso por role

--- MF API/Controllers/UsuariosController.cs
+++ MF API/Controllers/UsuariosController.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace MF_API.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class UsuariosController : ControllerBase
